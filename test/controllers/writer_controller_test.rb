47d429e130f94386ef08f5dd726a167569d53a24
Wan Ying Tsui <wanytsui@gmail.com>
2018-03-10 14:22:08 -0500

    Add and update tests for writer controller.

--- test/controllers/writer_controller_test.rb
+++ test/controllers/writer_controller_test.rb
@@ -3,12 +3,22 @@ require 'byebug'
 
 class WriterControllerTest < ActionDispatch::IntegrationTest
   setup do
-    writers(:default_writer)
-    stories(:default_writer_stories)
+    writer = Writer.create!(username: 'writer_test_1', password: ENV['seed_file_writer_default_password'], bio: "This is a writer created as part of a test.", email: ENV['seed_file_writer_default_email'])
+    writer.stories.create!(title: 'The Programmer and the Rock Dove', description: 'A young rock dove decides to program.', share_work: true)
   end
 
-  test "index" do
+  test "index is successful" do
     get writers_path
+    assert_response :success
+  end
+
+  test "new writer sign up is successful" do
+    get new_writer_registration_path
+    assert_response :success
+  end
+
+  test "new writer sign in is successful" do
+    get new_writer_session_path
     assert_response :success
   end
 end
