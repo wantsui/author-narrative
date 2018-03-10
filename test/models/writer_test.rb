6fafac2b4f06c894177afc110c732460dde0e604
Wan Ying Tsui <wanytsui@gmail.com>
2018-03-10 11:07:44 -0500

    Start adding tests for writers to test model.

--- test/models/writer_test.rb
+++ test/models/writer_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class WriterTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "doesn't create a writer with no fields" do
+    writer = Writer.new
+    assert_not writer.valid?, "Writer valid"
+  end
+  test "creates a writer when all fields provided" do
+    writer = Writer.new(username: 'writer_test1', email: 'sample_email@fakedomain.com', password: 'sample_password')
+    assert writer.valid?
+    assert writer.save
+  end
 end
