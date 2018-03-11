59a62851e3a4432007a99511e2e817ce432f5629
Wan Ying Tsui <wanytsui@gmail.com>
2018-03-11 19:18:00 -0400

    Add story model tests and story controller tests for the get route.

--- test/controllers/story_controller_test.rb
+++ test/controllers/story_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
-
+require 'byebug'
 class StoryControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @writer = Writer.create!(username: 'writer_test_1', password: ENV['seed_file_writer_default_password'], bio: "This is a writer created as part of a test.", email: ENV['seed_file_writer_default_email'])
+    @writer.stories.create!(title: 'The Programmer and the Rock Dove', description: 'A young rock dove decides to program.', share_work: true)
+  end
+
+  test "the writer's profile contains the correct bio information" do
+    get "/writers/#{@writer.id}"
+    assert response.body.include?(@writer.bio)
+    assert response.body.include?(@writer.username)
+    assert_response :success
+  end
 end
--- test/models/story_test.rb
+++ test/models/story_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
-
 class StoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "doesn't create a story given 0 fields" do
+    story = Story.new
+    assert_not story.valid?, "Writer valid"
+  end
+
+  test "creates a writer when all fields provided" do
+    writer = Writer.create(username: 'writer_test1', email: 'sample_email@fakedomain.com', password: 'sample_password')
+    story = writer.stories.create(title: "The best title", description: "I don't know yet")
+    assert story.valid?
+    assert story.save
+    assert Story.count == 1
+    assert Story.first.writer.username == writer.username
+  end
 end
